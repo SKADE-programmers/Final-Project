6185933eeafea042c8fbc53b0cabe6bea3d062b5
Kurpotlar <46979029+Kurpotlar@users.noreply.github.com>
2019-11-17 23:06:46 -0500

    Add files via upload

--- SKADE/FinalProject/FinalProject/LoginPage.cs
+++ SKADE/FinalProject/FinalProject/LoginPage.cs
@@ -13,38 +13,70 @@ using ClassLibrary;
 
 namespace FinalProject
 {
+
     public partial class LoginPage : Form
     {
-               
+        
+
         public LoginPage()
         {
             InitializeComponent();
         }
+        public int attempts = 1;
 
         private void Button1_Click(object sender, EventArgs e)
         {
             Logins login = new Logins();
             AuthenticateTableAdapter taAuth = new AuthenticateTableAdapter();
-            login.userName = textBox1.Text;
-            login.passWord = textBox2.Text;
+            login.userName = userText.Text;
+            login.passWord = userPass.Text;
             try
             {
-                DataTable dt = new DataTable();
+                DataTable dt = new DataTable();     
                 taAuth.Fill(authDS.Authenticate, login.userName, login.passWord);
                 if (authDS.Tables[0].Rows.Count == 1)
                 {
-                    label3.Text = "Success!";
+                    Ready_Label.Text = "Login Success!";
+                    MessageBox.Show("Welcome " + " " + login.userName + " :)");
+                    Main main = new Main();
+                    //this.Hide();
+                    main.Show();
                     
-                   // Application.Run(new Main());
+                    
+
+                    
                 }
-                else { label3.Text = "Failure!"; }
+                else {
+                    Ready_Label.Text = "login unsucessful\n" + attempts 
+                 + "out of 3";
+                    attempts++;
+                }
+
+                if (login.userName == userText.Text && login.passWord == "")
+                {
+                    Ready_Label.Text = "Please enter the password";
+                }
+                
+                if  (login.userName == "" && login.passWord == "")
+                {
+                    Ready_Label.Text = "Please enter the username and password";
+                }
+                
+                
+
             }
             catch (Exception ex)
             {
-                label3.Text = ex.Message;
+                Ready_Label.Text = "Something went wrong... please try again\n"  + ex.Message;
+            }
+            if(attempts == 4)
+            {
+                
+                MessageBox.Show("You have reached maximum number of attempts... ");
             }
 
         }
+        
 
         private void LoginPage_Load(object sender, EventArgs e)
         {
@@ -54,10 +86,17 @@ namespace FinalProject
 
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+       
+
+        private void forgotPassword_Click_1(object sender, EventArgs e)
         {
+            forgotPassword forgot = new forgotPassword();
+            forgot.Show();
+        }
 
-
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }

c545fda64c8eb061c28c495b79eff023116d4050
Kurpotlar <46979029+Kurpotlar@users.noreply.github.com>
2019-11-28 19:20:23 -0500

    Add files via upload

---
+++ SKADE/ClassLibrary/ClassLibrary/Custom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Custom
+    {
+        public int id { get;  }
+        public string cusFirstName { get; set; }
+        public string cusLastName { get; set; }
+        public string phone { get; set; }
+        public string cusAddress { get; set; }
+        public string cusCity { get; set; }
+        public string cusPostal { get; set; }
+        public string cusEmail { get; set; }
+
+
+
+    }
+}

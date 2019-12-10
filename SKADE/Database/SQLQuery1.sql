c68b474e826d421735457f9dcd655b8ba3486675
Kurpotlar <46979029+Kurpotlar@users.noreply.github.com>
2019-12-10 15:46:14 -0500

    Add files via upload

--- SKADE/Database/SQLQuery1.sql
+++ SKADE/Database/SQLQuery1.sql
@@ -19,3 +19,20 @@ INSERT INTO logins (empID, logUser, logPass) VALUES (7, 'erosten', 'pass');
 select logins.id, empID, logUser, logPass, employee.id, employee.empFirst + ' ' + employee.empLast AS 'Employee'  
 from logins inner join employee on employee.id = logins.empID
 
+Create Table Requests -- data below
+(
+	id int IDENTITY(1,1),
+	requestDate Datetime NOTNULL,
+	requestState bool,
+	posID int NOT NULL,
+	empID int,
+	order_lineID
+	
+	CONSTRAINT Requests_pk_id PRIMARY KEY(id),
+	CONSTRAINT logins_fk_empID FOREIGN KEY(empID) REFERENCES employee(id),
+	CONSTRAINT logins_fk_posID FOREIGN KEY(posID) REFERENCES position(id),
+	CONSTRAINT logins_fk_order_lineID FOREIGN KEY(order_lineID) REFERENCES order_line(id)
+);
+
+
+

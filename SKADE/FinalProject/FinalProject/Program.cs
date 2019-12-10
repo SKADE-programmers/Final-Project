d92dcd92024cbc5f06110b4218d57fd8e20dd11f
ShadowsHuntUs <57641833+ShadowsHuntUs@users.noreply.github.com>
2019-12-10 15:45:48 -0500

    Add files via upload

--- SKADE/FinalProject/FinalProject/Program.cs
+++ SKADE/FinalProject/FinalProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FinalProject;
 
 namespace FinalProject
 {
@@ -17,7 +18,9 @@ namespace FinalProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //for testing:
-            //Application.Run(new EmployeDetailForm());
+
+            Application.Run(new Sales.CustomerHistory());
+            Application.Run(new WeeklyReport());
             //Application.Run(new Customer());
             //Application.Run(new Sales.Sale());
             Application.Run(new LoginPage());
